8bf1e9f21fe1545dcba673a74cbeb9ec77b49f28
Jayswal  Ajay <118416940+Ajay3010@users.noreply.github.com>
2025-06-25 10:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function fibonacci(num) {
-  if (num === 1) return 0;     // 1st Fibonacci number is 0
-  if (num === 2) return 1;     // 2nd Fibonacci number is 1
+  if (num <= 0) return -1; // Invalid input
+  if (num === 1) return 0;
+  if (num === 2) return 1;
 
   let a = 0, b = 1;
   for (let i = 3; i <= num; i++) {
-    let c = a + b;
+    let next = a + b;
     a = b;
-    b = c;
+    b = next;
   }
   return b;
 }
